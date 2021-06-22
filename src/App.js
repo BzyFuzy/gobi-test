7b31c2d35ebe3995b00d547ca38f0c7ef21cac8b
fromHome <bzy.fuzy@gmail.com>
2021-06-23 02:09:58 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import NavigationBar from './components/navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavigationBar />
+			<div className="header">
+				<div className="top row center">
+					<ul className="menu row center">
+						<li>WOMEN</li>
+						<li>MEN</li>
+						<li>HOME</li>
+						<li>ACCESSORIES</li>
+						<li style={{ marginRight: 0 }}>ORGANIC</li>
+					</ul>
+					<div className="logo">
+						<img src="/img/logo.svg" height="42px" />
+					</div>
+					<div className="row center" style={{ flex: 2, display: "flex", justifyContent:"flex-end" }}>
+						<i className="icon search" />
+						<input className="search" placeholder="Search" />
+						<i className="icon heart" />
+						<i className="icon bag" />
+					</div>
+          
+				</div>
+        <div style={{display: "flex", background: "cyan"}}>
+          <div style={{flex: 1}}></div>
+          <div style={{flex: 1, background: "green"}}>
+          <p className="summer">SUMMER </p>
+          <p className="sale">Sale</p>
+          </div>
+        </div>
+			</div>
+
+      
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,23 @@
+const NavigationBar = () => (
+	<div className="App">
+		<div className="nav">
+			<div className="row center">
+				<i className="icon earth" />
+				<p>Global - English</p>
+			</div>
+			<div className="row center">
+				<p style={{ marginRight: 14 }}>RETURNING AN ORDER?</p>
+				<a href="#re">Click for more info</a>
+			</div>
+			<div className="row center" >
+				<p className="button" style={{ marginRight: '22.98px' }}>
+					SIGN IN
+				</p>
+				<p className="button">SIGN UP</p>
+			</div>
+		</div>
+        
+	</div>
+);
+
+export default NavigationBar;
