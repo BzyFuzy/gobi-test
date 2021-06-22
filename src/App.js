da1e7cd8d9ad959eba47975df626ea9d8575dde0
fromHome <bzy.fuzy@gmail.com>
2021-06-23 03:05:52 +0800

    header

--- src/App.js
+++ src/App.js
@@ -24,11 +24,17 @@ function App() {
 					</div>
           
 				</div>
-        <div style={{display: "flex"}}>
+        <div style={{display: "flex", flex:1}}>
           <div style={{flex: 1}}></div>
-          <div style={{flex: 1}}>
+          <div className="special" >
           <p className="summer">SUMMER </p>
           <p className="sale">Sale</p>
+          <div className="off row center" >
+            <p className="upto">upto</p>
+            <p className="stp">60%</p>
+            <p className="ff">ff</p>
+          </div>
+          <button>shop now</button>
           </div>
         </div>
 			</div>
